2ddaca20f641f3afcf37ed7a3afa0b6c23c02582
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2025-11-03 16:14:40 +0100

    TW - Started experimenting with a loading-screen. Needs optimizing.

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -4,16 +4,34 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// Initialize a new Lenis instance for smooth scrolling
+
+
+// Loading Screen Animation
+const loadingScreen = document.querySelector(".loading-screen");
+
+// Has the user visited already? No? Execute the function.
+if (!sessionStorage.getItem('visited')) {
+	
+	// Mark the user as visited.
+	sessionStorage.setItem('visited', 'true');
+
+  	window.addEventListener('load', () => {
+    	loadingScreen.classList.add("play-loader");
+  	});
+}
+
+
+
+// Lenis Smooth Scrolling
 const lenis = new Lenis();
 
-// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+// Synchronize Lenis with GSAP's ScrollTrigger Plugin
 lenis.on('scroll', ScrollTrigger.update);
 
 // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
 // This ensures Lenis's smooth scroll animation updates on each GSAP tick
 gsap.ticker.add((time) => {
-  lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  	lenis.raf(time * 750); // Convert time from seconds to milliseconds
 });
 
 // Disable lag smoothing in GSAP to prevent any delay in scroll animations
