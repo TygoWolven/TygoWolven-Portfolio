41d0c6d85396013674fb4c72f5e4c7a5cec7015f
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2025-11-04 14:25:42 +0100

    TW - Quick test with the preload link.

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,56 +1,6 @@
 import Lenis from "lenis";
-import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-
-
-
-
-
-// -=-=-=- PRELOADER -=-=-=- //
-
-const counterElement = document.querySelector(".counter"),
-      overlayElement = document.querySelector(".overlay");
-
-// Has the user visited already? No? Execute the function.
-if (!sessionStorage.getItem('visited')) {
-	
-	// Mark the user as visited.
-	sessionStorage.setItem('visited', 'true');
-	
-	// Show the preloader-elements since they are hidden by default.
-	counterElement.style.display = "flex";
-	overlayElement.style.display = "flex";
-
-	startLoader();
-}
-
-function startLoader() {
-  	let currentValue = 0;
-
-  	function updateCounter() {
-  	  	if (currentValue >= 100) return;
-  	  	currentValue++;
-  	  	counterElement.textContent = currentValue;
-  	  	setTimeout(updateCounter, 20); // 20ms delays.
-  	}
-
-  	updateCounter();
-
-	gsap.to(".counter", 0.25, {
-		delay: 3,
-		opacity: 0,
-	});
-	  
-	gsap.to(".bar", 1.5, {
-		delay: 3,
-		height: 0,
-		stagger: {
-			amount: 0.5,
-		},
-		ease: "power4.inOut",
-	});
-}
 
 
 
