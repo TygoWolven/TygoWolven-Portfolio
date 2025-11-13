fe1ecf7c935ff9fd62e4b0815b78e70db783c173
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2025-11-13 17:16:07 +0100

    TW - Added a text-animation.

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,7 +1,81 @@
+// Import Lenis
 import Lenis from "lenis";
+
+// Import GSAP
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+import { SplitText } from "gsap/SplitText";
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+
+
+
+
+// -=-=-=- LENIS SMOOTH SCROLLING -=-=-=- //
+
+const lenis = new Lenis();
+
+// Synchronize Lenis with GSAP"s ScrollTrigger Plugin
+lenis.on("scroll", ScrollTrigger.update);
+
+// Add Lenis"s requestAnimationFrame (raf) method to GSAP"s ticker
+// This ensures Lenis"s smooth scroll animation updates on each GSAP tick
+gsap.ticker.add((time) => {
+  	lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+});
+
+// Disable lag smoothing in GSAP to prevent any delay in scroll animations
+gsap.ticker.lagSmoothing(0);
+
+
+
+
+
+// -=-=-=- HEADING ANIMATION ON SCROLL -=-=-=- //
+
+const animatedHeading = document.querySelector(".animated-heading"),
+      originalHeading = animatedHeading.textContent.trim();
+  
+// Duplicate the text to create a loop
+animatedHeading.innerHTML = `${originalHeading} - ${originalHeading}`;
+
+gsap.to(animatedHeading, {
+  	scrollTrigger: {
+  	  	trigger: animatedHeading, // Animate when "animatedHeading" is in view.
+  	  	start: "top bottom", // Top of element reaches bottom of screen.
+  	  	end: "bottom top", // Bottom of element reaches top of screen.
+  	  	scrub: true, // Smoothly scrub the animation with the scroll
+  	},
+  	x: "-50%",
+  	ease: "none", 
+});
+
+
+
+
+
+// -=-=-=- PARAGRAPH ANIMATION ON SCROLL -=-=-=- //
+
+const animatedParagraph = document.querySelector(".animated-paragraph");
+
+const splittedParagraph = new SplitText(animatedParagraph, {
+    type: "lines",
+	mask: "lines",
+});
+
+gsap.from(splittedParagraph.lines, {
+  	scrollTrigger: {
+  	  	trigger: animatedParagraph,
+  	  	start: "top 75%",
+  	  	toggleActions: "play none none reverse" // Play on enter, reverse on the way back.
+  	},
+  	yPercent: 150,
+  	duration: 1,
+  	ease: "power4.out",
+  	stagger: {
+  	  	each: 0.1
+  	},
+});
 
 
 
@@ -16,44 +90,4 @@ document.addEventListener("visibilitychange", () => {
     } else {
         document.title = originalTitle;
     }
-});
-
-
-
-
-
-// -=-=-=- LENIS SMOOTH SCROLLING -=-=-=- //
-
-const lenis = new Lenis();
-
-// Synchronize Lenis with GSAP's ScrollTrigger Plugin
-lenis.on('scroll', ScrollTrigger.update);
-
-// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
-// This ensures Lenis's smooth scroll animation updates on each GSAP tick
-gsap.ticker.add((time) => {
-  	lenis.raf(time * 1000); // Convert time from seconds to milliseconds
-});
-
-// Disable lag smoothing in GSAP to prevent any delay in scroll animations
-gsap.ticker.lagSmoothing(0);
-
-
-
-const animatedText = document.querySelector('.animated-text'),
-      originalText = animatedText.textContent.trim();
-  
-// Duplicate the text for a seamless loop
-animatedText.innerHTML = `${originalText} - ${originalText}`;
-
-// Apply GSAP ScrollTrigger animation
-gsap.to(animatedText, {
-  scrollTrigger: {
-    trigger: animatedText, // Animate when this element is in view
-    start: "top bottom", // Start when the element is just entering the viewport
-    end: "bottom top", // End when the element leaves the viewport
-    scrub: true, // Smoothly scrub the animation with the scroll
-  },
-  x: "-50%", // Move the text to the left
-  ease: "none", // Keep a constant speed
 });
