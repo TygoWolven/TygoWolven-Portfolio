b8f63d48a09ca77d231847567e0e626f6c6518b6
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2025-11-19 17:09:37 +0100

    TW - Some tiny adjustments.

--- src/assets/scripts/preloader.js
+++ src/assets/scripts/preloader.js
@@ -2,8 +2,7 @@ import { gsap } from "gsap";
 
 // -=-=-=- PRELOADER -=-=-=- //
 
-const counterElement = document.querySelector(".counter"),
-      overlayElement = document.querySelector(".overlay");
+const preloaderElement = document.querySelector(".preloader");
 
 // Has the user visited already? No? Execute the function.
 if (!sessionStorage.getItem('visited')) {
@@ -12,31 +11,23 @@ if (!sessionStorage.getItem('visited')) {
 	sessionStorage.setItem('visited', 'true');
 	
 	// Show the preloader-elements since they are hidden by default.
-	counterElement.style.display = "flex";
-	overlayElement.style.display = "flex";
+	preloaderElement.style.display = "flex";
 
 	startLoader();
 }
 
 function startLoader() {
-  	let currentValue = 0;
-
-  	function updateCounter() {
-  	  	if (currentValue >= 100) return;
-  	  	currentValue++;
-  	  	counterElement.textContent = currentValue;
-  	  	setTimeout(updateCounter, 20); // 20ms delays.
-  	}
-
-  	updateCounter();
-
-	gsap.to(".counter", 0.25, {
+	gsap.to(".heading", 0.25, {
 		delay: 3,
 		opacity: 0,
 	});
+	gsap.to(".content", 0.25, {
+		delay: 3.2,
+		opacity: 0,
+	});
 	  
-	gsap.to(".bar", 1.5, {
-		delay: 3,
+	gsap.to(".preloader", 1.5, {
+		delay: 3.2,
 		height: 0,
 		stagger: {
 			amount: 0.5,
